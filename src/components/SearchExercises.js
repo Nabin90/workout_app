9300879e4c9f261f23a0b519f7ab383c7e743097
Nabin90 <nabinpunis90@gmail.com>
2022-06-17 15:23:25 +0545

    search functionality setup

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
+import {exerciseOptions, fetchData} from '../utils/fetchData';
+
 const SearchExercises = () => {
 
+  const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+
+  const handleSearch = async() => {
+    if(search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.fitler((exercise) => 
+        exercise.name.toLowerCase().includes(search)
+        || exercise.target.toLowerCase().includes(search)
+        || exercise.equipment.toLowerCase().includes(search)
+        || exercise.bodyPart.toLowerCase().includes(search)
+      );
+
+      setSearch('');
+      setExercises(searchedExercises);
+    }
+  }
 
   return (
     <Stack
@@ -37,8 +57,8 @@ const SearchExercises = () => {
             backgroundColor: '#fff',
           }}
           height="70px"
-          value=''
-          onChange={(e) => {}}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Workouts"
           type="text"
         />
@@ -54,6 +74,7 @@ const SearchExercises = () => {
             position: 'absolute',
             right: '0'
           }}
+          onClick={handleSearch}
         >
           Search
         </Button>
