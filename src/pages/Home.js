cf7567fb62cf845b89e52b9ba9af08d9e868f911
Nabin90 <nabinpunis90@gmail.com>
2022-06-18 15:00:48 +0545

    categories setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,21 @@ import {Box} from '@mui/material';
 import {HeroBanner, SearchExercises, Exercises} from '../components';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises 
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises 
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   )
 }
