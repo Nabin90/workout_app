a0b1ab698bfabfcdb4db053a36dc7be6f3aacd2c
Nabin90 <nabinpunis90@gmail.com>
2022-06-18 16:16:27 +0545

    basic gif introduction

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,6 +3,7 @@ import Pagination from '@mui/material/Pagination';
 import {Box, Stack, Typography} from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import {ExerciseCard} from '../components';
 
 const Exercises = ({ exercises, setExercises, bodyPart}) => {
 console.log(exercises);
@@ -14,17 +15,17 @@ console.log(exercises);
       mt="50px"
       p="20px"
     >
-      <Typography variant="h3" mb="46px">
+      <Typography variant="h3" mb="46px" ml="20px">
         Showing Results
       </Typography>
       <Stack
         direction="row"
-        sx={{ gap: { lg: '110px', xs: '50px' }}}
+        sx={{ gap: { lg: '100px', xs: '50px' }}}
         flexWrap="wrap"
         justifyContent="center"
       >
         {exercises.map((exercise, index) => (
-          <p>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
     </Box>
