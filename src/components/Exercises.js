84e7ae76d9904c94e4202401fc1f6a5d2d3b04f5
Nabin90 <nabinpunis90@gmail.com>
2022-06-19 14:03:42 +0545

    pagination added

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,7 +6,18 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import {ExerciseCard} from '../components';
 
 const Exercises = ({ exercises, setExercises, bodyPart}) => {
-console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 6;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1450, behavior: 'smooth' });
+  }
 
   return (
     <Box
@@ -24,9 +35,22 @@ console.log(exercises);
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+          {exercises.length > exercisesPerPage && (
+            <Pagination 
+              color="standard"
+              shape="rounded"
+              defaultPage={1}
+              count={Math.ceil(exercises.length/exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+            />
+          )}
       </Stack>
     </Box>
   )
